6c4a8721ef6158ab7121d9a3bc2196d8bc7ed13d
Jacob Beck <jake@fishtownanalytics.com>
2018-12-05 12:29:17 -0700

    Initial python-only pass
    Split out setup.py files, make adapters separate packages
    Make local tests w/ docker work at least
    TODO: macros are still in the 'core' package.
    TODO: all tests are still in the 'core' package.

--- core/setup.py
+++ core/setup.py
@@ -6,20 +6,10 @@ import os
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
-package_name = "dbt"
-package_version = "0.12.2a1"
+package_name = "dbt-core"
+package_version = "0.13.0a0"
 description = """dbt (data build tool) is a command line tool that helps \
 analysts and engineers transform data in their warehouse more effectively"""
-
-
-boto_requirements = [
-    'boto3>=1.6.23,<1.8.0',
-    'botocore>=1.9.23,<1.11.0',
-]
-
-postgres_requirements = [
-    'psycopg2>=2.7.5,<2.8',
-]
 
 
 setup(
@@ -50,15 +40,6 @@ setup(
     scripts=[
         'scripts/dbt',
     ],
-    extras_require={
-        # when we split out adapters into their own python packages, these
-        # extras requirements lists will just be the appropriate adapter
-        # packages
-        'snowflake': ['snowflake-connector-python>=1.4.9'] + boto_requirements,
-        'bigquery': ['google-cloud-bigquery>=1.0.0,<2'],
-        'redshift': boto_requirements[:] + postgres_requirements[:],
-        'postgres': postgres_requirements[:],
-    },
     install_requires=[
         'Jinja2>=2.10',
         'PyYAML>=3.11',
