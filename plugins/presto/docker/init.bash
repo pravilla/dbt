ee43002efc3064e9437f18afd741295cf3483b78
Jacob Beck <jake@fishtownanalytics.com>
2018-12-11 12:39:23 -0700

    ugh idk

--- plugins/presto/docker/init.bash
+++ plugins/presto/docker/init.bash
@@ -1,5 +1,11 @@
 #!/bin/bash
+
+# move to wherever we are so docker things work
+cd "$(dirname "${BASH_SOURCE[0]}")"
+
 set -exo pipefail
+
+docker-compose build
 docker-compose up -d hive-metastore-db
 
 docker-compose -f util.yml run --rm util wait_for_up hive-metastore-db 5432
@@ -7,6 +13,3 @@ docker-compose run --rm hive-hiveserver ./bin/schematool -initSchema -dbType pos
 docker-compose up -d hive-metastore
 docker-compose -f util.yml run --rm util wait_for_up hive-metastore 9083
 docker-compose up -d
-# this one can take a while
-docker-compose -f util.yml run --rm util wait_for_up hive-hiveserver 10000 10
-docker-compose -f util.yml run --rm util wait_for_up presto 8080
